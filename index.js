b15e63dc7aef9aeb415a65b942b22f96f15a43b1
Esben Holk <esbenholknielsen@gmail.com>
2020-04-05 11:57:08 +0200

    set up for tarot online experience

--- index.js
+++ index.js
@@ -15,25 +15,50 @@ app.use(express.static("./public"));
 app.use(express.static("./utils"));
 
 app.get("/", (req, res) => {
-    res.render("frontpage", {
-        layout: "main"
-    });
+  res.render("frontpage", {
+    layout: "main"
+  });
 });
-app.get("/tarotcards", (req, res) => {
-    res.render("tarotcards", {
-        layout: "main"
-    });
+
+const images = [
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotcardreader.png",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotcardreader2.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotcardsontable.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotreading.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/+++.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/IMG_2130.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/IMG_2156.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/iheartberlinevent1.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/iheartberlinevent3.jpg",
+  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/fan2ficballhaus0572rohmk.jpg"
+];
+
+app.get("/3d", (req, res) => {
+  res.render("virtualexperience", {
+    layout: "main",
+    imagearray: images
+  });
 });
-app.get("/documentation", (req, res) => {
-    res.render("documentation", {
-        layout: "main"
-    });
-});
-app.get("/merch", (req, res) => {
-    res.render("documentation", {
-        layout: "main"
-    });
-});
+// app.get("/tarotcards", (req, res) => {
+//   res.render("tarotcards", {
+//     layout: "main"
+//   });
+// });
+// app.get("/documentation", (req, res) => {
+//   res.render("documentation", {
+//     layout: "main"
+//   });
+// });
+// app.get("/merch", (req, res) => {
+//   res.render("documentation", {
+//     layout: "main"
+//   });
+// });
+// app.get("/livestream", (req, res) => {
+//   res.render("livestream", {
+//     layout: "main"
+//   });
+// });
 
 // app.use((request, response, next) => {
 //     if (request.url != "/register" && request.url != "/login") {
