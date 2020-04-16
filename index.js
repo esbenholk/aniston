9470f10dbd6dbdb0ff6317015eaa3cd19c33bba1
Esben Holk <esbenholknielsen@gmail.com>
2020-04-16 18:09:05 +0200

    testing livestream

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-
 const databaseActions = require("./utils/database");
 // const querystring = require("querystring");
 const hb = require("express-handlebars");
 const cookieSession = require("cookie-session");
 const { hash, compare } = require("./utils/bcrypt");
 const csurf = require("csurf");
+const archive = require("./public/data.json");
+
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 app.engine("handlebars", hb()); //handlebars is construction languae
 app.set("view engine", "handlebars"); //handlebar is templating language
@@ -14,61 +17,46 @@ app.use(express.static("./views"));
 app.use(express.static("./public"));
 app.use(express.static("./utils"));
 
+io.on("connection", socket => {
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chatmessage", msg => {
+    console.log("message: " + msg);
+    io.emit("chatmessage", msg);
+  });
+  socket.on("livestream", video => {
+    console.log("reciving");
+    console.log("livestream", video);
+    io.emit("livestream", video);
+  });
+});
+
 app.get("/", (req, res) => {
   res.render("frontpage", {
     layout: "main"
   });
 });
 
-const images = [
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotcardreader.png",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotcardreader2.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotcardsontable.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/tarotreading.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/+++.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/IMG_2130.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/IMG_2156.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/iheartberlinevent1.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/iheartberlinevent3.jpg",
-  "https://houseofkillingwebsite.s3.amazonaws.com/aniston/fan2ficballhaus0572rohmk.jpg"
-];
-
-app.get("/3d", (req, res) => {
+app.get("/JenniferAnistonSuperfansDigitalTarotLandscape", (req, res) => {
   res.render("virtualexperience", {
-    layout: "main",
-    imagearray: images
+    layout: "main"
   });
 });
-// app.get("/tarotcards", (req, res) => {
-//   res.render("tarotcards", {
-//     layout: "main"
-//   });
-// });
-// app.get("/documentation", (req, res) => {
-//   res.render("documentation", {
-//     layout: "main"
-//   });
-// });
-// app.get("/merch", (req, res) => {
-//   res.render("documentation", {
-//     layout: "main"
-//   });
-// });
-// app.get("/livestream", (req, res) => {
-//   res.render("livestream", {
-//     layout: "main"
-//   });
-// });
+app.get("/archive", (req, res) => {
+  res.render("5thdimension", {
+    layout: "main"
+  });
+});
+app.get("/emojicon", (req, res) => {
+  res.render("emojicon", {
+    layout: "secondary"
+  });
+});
+app.get("/webcam-landscape", (req, res) => {
+  res.render("livestream", {
+    layout: "main"
+  });
+});
 
-// app.use((request, response, next) => {
-//     if (request.url != "/register" && request.url != "/login") {
-//         response.render("frontpage", {
-//             layout: "main"
-//         });
-//     } else {
-//         console.log("redirecting through url");
-//         response.redirect("/");
-//     }
-// });
-
-app.listen(process.env.PORT || 8080, () => console.log("awake"));
+http.listen(process.env.PORT || 8080, () => console.log("awake"));
