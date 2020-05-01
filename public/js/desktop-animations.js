99904eefda96d7abc4da003b7dce3fcb708c13fb
Esben Holk <esbenholknielsen@gmail.com>
2020-05-01 19:04:56 +0200

    fixed click handler for open windows with extra desktop-div and better control of parentNode

--- public/js/desktop-animations.js
+++ public/js/desktop-animations.js
@@ -1,30 +1,50 @@
+// click handler for document to close clicked window
 document.addEventListener("click", function(target) {
   for (var i = 0; i < target.target.classList.length; i++) {
     if (target.target.classList[i] === "windowbutton") {
       target.target.parentNode.parentNode.parentNode.classList.add("invisible");
       target.target.parentNode.parentNode.parentNode.classList.remove("open");
+      if (
+        target.target.parentNode.parentNode.parentNode.getElementsByTagName(
+          "video"
+        ).length > 0
+      ) {
+        let videos = target.target.parentNode.parentNode.parentNode.getElementsByTagName(
+          "video"
+        );
+        for (var i = 0; i < videos.length; i++) {
+          videos[i].pause();
+        }
+      }
     }
   }
 });
 
-createTrigger("emojicon", "emojiconwindow", "emojiconheader");
-createTrigger("stayvirtual", "stayvirtualwindow", "stayvirtualheader");
-$(function() {
-  $("#emojiconwindow").resizable();
-});
-$(function() {
-  $("#stayvirtualwindow").resizable();
+let currentWindow = {};
+let otherWindow = {};
+var isDown = false;
+var offset = [0, 0];
+var mousePosition;
+
+$(".windowHeader").mousedown(function(e) {
+  console.log(e.target.parentNode.id);
+  currentWindow = document.getElementById(e.target.parentNode.parentNode.id);
+  if (currentWindow.classList.contains("window")) {
+    currentWindow.classList.add("currentWindow");
+    isDown = true;
+    offset = [
+      currentWindow.offsetLeft - e.clientX,
+      currentWindow.offsetTop - e.clientY
+    ];
+  }
 });
 
-var mousePosition;
-var offset = [0, 0];
-var div;
-var isDown = false;
-var currentWindow = {};
 document.addEventListener(
   "mouseup",
   function() {
     isDown = false;
+    otherWindow = currentWindow;
+    currentWindow = {};
   },
   true
 );
@@ -37,28 +57,22 @@ document.addEventListener(
         x: event.clientX,
         y: event.clientY
       };
+      currentWindow.style.zIndex = "999";
       currentWindow.style.left = mousePosition.x + offset[0] + "px";
       currentWindow.style.top = mousePosition.y + offset[1] + "px";
     }
   },
   true
 );
-moveWindow("emojiconheader");
-function moveWindow(name) {
-  let div = document.getElementById(name);
-  currentWindow = div.parentNode;
-  div.addEventListener(
-    "mousedown",
-    function(e) {
-      isDown = true;
-      offset = [
-        currentWindow.offsetLeft - e.clientX,
-        currentWindow.offsetTop - e.clientY
-      ];
-    },
-    true
-  );
-}
+
+createTrigger("emojicon", "emojiconwindow", "emojiconheader");
+createTrigger("stayvirtual", "stayvirtualwindow", "stayvirtualheader");
+
+$(function() {
+  $("#emojiconwindow").resizable();
+  $("#stayvirtualwindow").resizable();
+});
+
 function createTrigger(button, window, header) {
   let trigger = document.getElementById(button);
   trigger.addEventListener("click", function() {
@@ -67,9 +81,21 @@ function createTrigger(button, window, header) {
       targetwindow.classList.remove("invisible");
       targetwindow.classList.add("open");
       targetwindow.style.zIndex = "9999";
+      if (targetwindow.getElementsByTagName("video").length > 0) {
+        let videos = targetwindow.getElementsByTagName("video");
+        for (var i = 0; i < videos.length; i++) {
+          videos[i].play();
+        }
+      }
     } else {
       targetwindow.classList.remove("open");
       targetwindow.classList.add("invisible");
+      if (targetwindow.getElementsByTagName("video").length > 0) {
+        let videos = targetwindow.getElementsByTagName("video");
+        for (var i = 0; i < videos.length; i++) {
+          videos[i].pause();
+        }
+      }
     }
   });
 }
